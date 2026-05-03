c46f73eb7a26dec1cbe1e6bf14fe5f17cc91cb63
MD Ibna Abid <mdabout2@gmail.com>
2026-05-03 06:54:33 +0600

    footer designed

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,10 @@
+import dns from "node:dns";
+dns.setServers (["8.8.8.8","8.8.4.4"])
+
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "./Navbar/page";
+import Footer from "./Footer/page";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -21,12 +25,17 @@ export default function RootLayout({ children }) {
   return (
     <html
       lang="en"
-      data-theme="light"
+       data-theme="light"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">
+      <body className="min-h-full bg-yellow-100 flex flex-col">
+        
         <Navbar></Navbar>
-        {children}</body>
+       
+        {children}
+         <Footer></Footer>
+        
+        </body>
     </html>
   );
 }
--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,16 @@
-import Image from "next/image";
+import AllCards from "./Card/page";
+import HomePage from "./Home/page";
+import SummerTips from "./summerTips/page";
+import TopBrands from "./top-brand/page";
+
 
 export default function Home() {
   return (
-  <>
-  
-  </>
+    <>
+    <HomePage></HomePage>
+    <AllCards></AllCards>
+    <SummerTips></SummerTips>
+    <TopBrands></TopBrands>
+    </>
   );
 }
